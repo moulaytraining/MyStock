1101f2508594d33aaecb337e46f357089de6799e
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-05-19 02:55:55 +0100

    Add files via upload

---
+++ scripts/TextTransformation.js
@@ -0,0 +1,5 @@
+var myRwTxt=document.querySelector('#RwTxtArea');
+var PrprTxt=document.querySelector('#TrnsfrmdTxt1');
+var CmlCseTxt=document.querySelector('#TrnsfrmdTxt2');
+var TxtCollection=document.querySelector('#TrnsfrmdTxt3');
+var myText="";
