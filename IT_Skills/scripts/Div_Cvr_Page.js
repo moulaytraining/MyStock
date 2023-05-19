752a39999d64b02b4fff02d64635f713d9651c6a
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-05-19 05:15:46 +0100

    Update Div_Cvr_Page.js

--- IT_Skills/scripts/Div_Cvr_Page.js
+++ IT_Skills/scripts/Div_Cvr_Page.js
@@ -30,15 +30,3 @@ function mouRemoveClass(element, name) {
   }
   element.className = arr1.join(" ");
 }
-
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("entries_filter");
-var btns = btnContainer.getElementsByClassName("category_butn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
