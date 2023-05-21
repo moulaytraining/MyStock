b0301ace65554e30126bea1ada29b884707fd1fa
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-05-21 19:52:02 +0100

    Update cdcatalogMain.js

--- IT_Skills/scripts/cdcatalogMain.js
+++ IT_Skills/scripts/cdcatalogMain.js
@@ -5,11 +5,11 @@
 function GetFiles(){ 
   myTextElem1 = document.querySelector('article').appendChild(document.createElement('textarea'));
   myTextElem1.setAttribute("id", "xmlcode");
-  myTextElem1.style.display="none";
   fetch(file1).then(x=>x.text()).then(y=>myTextElem1.value = y);
+  myTextElem1.style.display="none";
 
   myTextElem2 = document.querySelector('article').appendChild(document.createElement('textarea'));
   myTextElem2.setAttribute("id", "xsltcode");
-  myTextElem2.style.display="none"; 
   fetch(file2).then(x=>x.text()).then(y=>myTextElem2.value = y);
+  myTextElem2.style.display="none"; 
 }
