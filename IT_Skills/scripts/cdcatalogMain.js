32e9c04cb76dd31bb83957b4cf7d75e83a4281f3
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-05-21 22:28:16 +0100

    Update cdcatalogMain.js

--- IT_Skills/scripts/cdcatalogMain.js
+++ IT_Skills/scripts/cdcatalogMain.js
@@ -2,24 +2,24 @@ function GetFiles(){
   var myTextElem1, myTextElem2,file,text; 
   
   myTextElem1 = document.querySelector('#xmlcode');
-  var file='https://moulaytraining.github.io/MyStock/IT_Skills/xmls/cdcatalog.xml';
-  GetFileData(file,"#xmlcode");
+  file='https://moulaytraining.github.io/MyStock/IT_Skills/xmls/cdcatalog.xml';
+    fetch(file)
+    .then((response) => response.text())
+    .then((text) => {
+     myTextElem1.innerHTML=text;
+    });  
   myTextElem1.style.display="none";
 
   myTextElem2 =  document.querySelector('#xsltcode');
-  var file='https://moulaytraining.github.io/MyStock/IT_Skills/xslts/cdcatalogStyle.txt';
-  GetFileData(file,"#xsltcode");
+   file='https://moulaytraining.github.io/MyStock/IT_Skills/xslts/cdcatalogStyle.txt';
+    fetch(file)
+    .then((response) => response.text())
+    .then((text) => {
+    myTextElem2.innerHTML=text;
+    }); 
   myTextElem2.style.display="none";   
-  
+}
   // Waiting for three seconds 
   const date = Date.now();
   let currentDate = null;
   do {currentDate = Date.now();} while (currentDate - date < 3000);}
-
-function GetFileData(file,ElemId){ 
-  fetch(file)
-    .then((response) => response.text())
-    .then((text) => {
-     document.querySelector(ElemId).innerHTML=text;
-    });  
-}
