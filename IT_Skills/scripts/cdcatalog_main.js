c12f7f8871745cd79e28a2f418c7aebd1392e696
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-05-21 09:13:05 +0100

    Create cdcatalog_main.js

---
+++ IT_Skills/scripts/cdcatalog_main.js
@@ -0,0 +1,17 @@
+function GetFiles(){ 
+const file1="https://moulaytraining.github.io/MyStock/IT_Skills/xmls/cdcatalog.xml";
+ const file1="https://moulaytraining.github.io/MyStock/IT_Skills/xslts/cdcatalog.xslt.txt";
+  
+ const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+   document.getElementById("xmlcode").value = this.responseText;
+  }
+  xhttp.open("GET", file1);
+  xhttp.send();
+  
+ const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+   document.getElementById("xsltcode").value = this.responseText;
+  }
+  xhttp.open("GET", file2);
+  xhttp.send();}
