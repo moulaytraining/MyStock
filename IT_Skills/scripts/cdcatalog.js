c58dfd31ab2febd1b4dfa48fe2e2def1671647fe
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-05-21 23:10:53 +0100

    Update and rename cdcatalogTrans.js to cdcatalog.js

--- IT_Skills/scripts/cdcatalog.js
+++ IT_Skills/scripts/cdcatalog.js
@@ -1,4 +1,29 @@
 function transformxml(){
+ var myTextElem1, myTextElem2,file,text; 
+  
+  myTextElem1 = document.querySelector('#xmlcode');
+  file="https://moulaytraining.github.io/MyStock/IT_Skills/xmls/cdcatalog.xml";
+    await fetch(file)
+    .then((response) => response.text())
+    .then((text) => {
+     myTextElem1.innerHTML=text;
+    });  
+  myTextElem1.style.display="none";
+  myTextElem2 =  document.querySelector('#xsltcode');
+   file="https://moulaytraining.github.io/MyStock/IT_Skills/xslts/cdcatalogStyle.txt";
+    await fetch(file)
+    .then((response) => response.text())
+    .then((text) => {
+    myTextElem2.innerHTML=text;
+    }); 
+  myTextElem2.style.display="none";   
+  
+  // Waiting for three seconds 
+  const date = Date.now();
+  let currentDate = null;
+  do {currentDate = Date.now();} 
+  while (currentDate - date < 3000);
+ 
 if (window.ActiveXObject)
  {
  // IE
