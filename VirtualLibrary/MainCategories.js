5f4ea914d9acc60184bd85020f918a026392bc90
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-20 21:19:19 +0100

    Update MainCategories.js

--- VirtualLibrary/MainCategories.js
+++ VirtualLibrary/MainCategories.js
@@ -3,3 +3,13 @@ var myElem=document.querySelector('.main');
 var myHtmlFile="https://moulaytraining.github.io/MyStock/VirtualLibrary/MainCategories.html";
 loadElement(myElem,myHtmlFile);
 }
+function loadElement(element,file) { 
+  fetch(file)
+    .then(response => response.text())
+    .then(html => {
+      element.innerHTML = html;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+} 
