b9089b817857996247e2dd5fe9d47696e823d00d
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-20 21:11:10 +0100

    Create MainCategories.js

---
+++ VirtualLibrary/MainCategories.js
@@ -0,0 +1,5 @@
+function mainPageData(){
+var myElem=document.querySelector('.main');
+var myHtmlFile="https://moulaytraining.github.io/MyStock/VirtualLibrary/MainCategories.html";
+loadElement(myElem,myHtmlFile);
+}
