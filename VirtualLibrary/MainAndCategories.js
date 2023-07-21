6e1b5f56b50f2f266fe9fe0c2f16dcbdcdf5a6db
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-21 04:16:07 +0100

    Update MainAndCategories.js

--- VirtualLibrary/MainAndCategories.js
+++ VirtualLibrary/MainAndCategories.js
@@ -80,6 +80,6 @@ function ShowCategory(elem){
   console.log(classFile,className); 
   var myElem=document.querySelector('.main');
   loadElement(myElem,classFile);
-  filterSelection('all'); 
-  viewKinds(); 
+  setTimeout(filterSelection('all'), 5000);
+  setTimeout(viewKinds(); , 5000);  
 }
