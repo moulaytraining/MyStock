031b87f1721351701af2234d71ca45b5b0ca1d94
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-21 01:40:27 +0100

    Update MainAndCategories.js

--- VirtualLibrary/MainAndCategories.js
+++ VirtualLibrary/MainAndCategories.js
@@ -22,7 +22,9 @@ if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
 if (c!="") {console.log(c);btnClick(elem);
 }
 } 
-function w3AddClass(element, name) { var i, arr1, arr2; arr1 = element.className.split(" "); arr2 = name.split(" "); for (i = 0; i < arr2.length; i++) { if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];} } } 
+function w3AddClass(element, name) { var i, arr1, arr2; 
+arr1 = element.className.split(" "); arr2 = name.split(" "); for (i = 0; i < arr2.length; i++) { if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];} } 
+} 
 function w3RemoveClass(element, name) { var i, arr1, arr2; arr1 = element.className.split(" "); arr2 = name.split(" "); for (i = 0; i < arr2.length; i++) { while (arr1.indexOf(arr2[i]) > -1) { arr1.splice(arr1.indexOf(arr2[i]), 1); } } element.className = arr1.join(" "); } 
 function btnClick(elem){
 console.log(elem);
