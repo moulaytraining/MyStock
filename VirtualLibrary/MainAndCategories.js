708cf7f25eaef2b6fb97543b5f9a828f112828fa
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-21 02:45:04 +0100

    Update MainAndCategories.js

--- VirtualLibrary/MainAndCategories.js
+++ VirtualLibrary/MainAndCategories.js
@@ -74,8 +74,8 @@ current.className=current.className.replace(" active", "");
 elem.className += " active"; 
 } 
 function ShowCategory(elem){
-  var classFile='https://moulaytraining.github.io/MyStock/VirtualLibrary/'+e.target.className+'.html';
-  var className=e.target.innerText;
+  var classFile='https://moulaytraining.github.io/MyStock/VirtualLibrary/'+elem.className+'.html';
+  var className=elem.innerText;
   console.log('Hello'); 
   console.log(classFile,className); 
   var myElem=document.querySelector('.main');
