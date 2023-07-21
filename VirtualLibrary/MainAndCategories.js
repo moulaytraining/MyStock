63bfaf7df221925369bbc41e23b09cebae676559
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-21 01:42:35 +0100

    Update MainAndCategories.js

--- VirtualLibrary/MainAndCategories.js
+++ VirtualLibrary/MainAndCategories.js
@@ -23,9 +23,24 @@ if (c!="") {console.log(c);btnClick(elem);
 }
 } 
 function w3AddClass(element, name) { var i, arr1, arr2; 
-arr1 = element.className.split(" "); arr2 = name.split(" "); for (i = 0; i < arr2.length; i++) { if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];} } 
+arr1 = element.className.split(" "); 
+arr2 = name.split(" "); 
+for (i = 0; i < arr2.length; i++) { 
+  if (arr1.indexOf(arr2[i]) == -1) {
+    element.className += " " + arr2[i];
+  } 
 } 
-function w3RemoveClass(element, name) { var i, arr1, arr2; arr1 = element.className.split(" "); arr2 = name.split(" "); for (i = 0; i < arr2.length; i++) { while (arr1.indexOf(arr2[i]) > -1) { arr1.splice(arr1.indexOf(arr2[i]), 1); } } element.className = arr1.join(" "); } 
+} 
+function w3RemoveClass(element, name) { 
+var i, arr1, arr2; 
+arr1 = element.className.split(" "); 
+arr2 = name.split(" "); for (i = 0; i < arr2.length; i++) { 
+  while (arr1.indexOf(arr2[i]) > -1) { 
+    arr1.splice(arr1.indexOf(arr2[i]), 1); 
+  } 
+} 
+  element.className = arr1.join(" "); 
+} 
 function btnClick(elem){
 console.log(elem);
 var btnContainer = document.getElementById("myBtnContainer");
