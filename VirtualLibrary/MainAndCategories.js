043e8ef5810f4847d489d6dc52ee50412076e5eb
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-22 02:05:52 +0100

    Update MainAndCategories.js

--- VirtualLibrary/MainAndCategories.js
+++ VirtualLibrary/MainAndCategories.js
@@ -19,8 +19,8 @@ if (c == "all") c = "";
 for (i = 0; i < x.length; i++) { w3RemoveClass(x[i], "show"); 
 if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show"); 
 }  
-if (c!="") {console.log(c);btnClick(elem);
-}
+btnClick(elem);
+//if (c!="") {console.log(c);}
 } 
 function w3AddClass(element, name) { 
   var i, arr1, arr2; 
@@ -66,7 +66,7 @@ var myElem;
 console.log(myCheckList);
 }
 function btnClick(elem){
-console.log(elem);
+//console.log(elem);
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 var current = document.getElementsByClassName("btn active")[0];
