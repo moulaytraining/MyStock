b65fc2bf04fccf7acb86dfdf10cd428258c4a141
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2023-07-22 02:41:17 +0100

    Update MainAndCategories.js

--- VirtualLibrary/MainAndCategories.js
+++ VirtualLibrary/MainAndCategories.js
@@ -15,7 +15,7 @@ loadElement(myElem,myHtmlFile);
 } 
 function filterSelection(c,elem) {
 var x, i; x = document.getElementsByClassName("column"); 
-//if (c == "all") c = ""; 
+if (c == "all") c = ""; 
 for (i = 0; i < x.length; i++) { w3RemoveClass(x[i], "show"); 
 if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show"); 
 }  
