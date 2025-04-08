d58a30fa58050a275e6d37ff72638069b107d5f4
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-08 02:24:01 +0200

    Update utilities.js

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -2,7 +2,7 @@ function getElemTextById(file,elemId) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     document.getElementById(elemId).innerHTML =this.responseText; 
+       document.getElementById(elemId).innerHTML =this.responseText; 
     }
   };
   xhttp.open("GET", file, true);
