a0a65c2199c9f3297de14a57d1fe70852b9271f6
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-08 08:48:50 +0200

    Update Read_Txt_XML.js

--- scripts/Read_Txt_XML.js
+++ scripts/Read_Txt_XML.js
@@ -1,4 +1,34 @@
 function loadTextFiles(){
 	var file="https://moulaytraining.github.io/MyStock/scripts/employee.js"; 
-    getElemTextById(file,"id");  
+    textHttpRequest(file,getTextData);  
+}
+function getTextData(xmlDoc){
+	var i;
+			//var xmlDoc = xml.responseXML;
+			var table =
+				`<tr><th>Firstname</th><th>Lastname</th>
+					<th>Title</th><th>Division</th>
+					<th>Building</th><th>Room</th>
+				</tr>`;
+			var x = xmlDoc.getElementsByTagName("employee");
+
+			// Start to fetch the data by using TagName
+			for (i = 0; i < x.length; i++) {
+				table += "<tr><td>" +
+					x[i].getElementsByTagName("firstname")[0]
+					.childNodes[0].nodeValue + "</td><td>" +
+					x[i].getElementsByTagName("lastname")[0]
+					.childNodes[0].nodeValue + "</td><td>" +
+					x[i].getElementsByTagName("title")[0]
+					.childNodes[0].nodeValue + "</td><td>" +
+					x[i].getElementsByTagName("division")[0]
+					.childNodes[0].nodeValue + "</td><td>" +
+					x[i].getElementsByTagName("building")[0]
+					.childNodes[0].nodeValue + "</td><td>" +
+					x[i].getElementsByTagName("room")[0]
+					.childNodes[0].nodeValue + "</td></tr>";
+			}
+
+			// Print the xml data in table form
+			document.getElementById("id").innerHTML = table;
 }
