2f624810a2cd1b7365177263fb6fdaab1868d372
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-08 08:50:04 +0200

    Update utilities.js

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,4 +1,4 @@
-function textHttpRequest(file,getTextData) {
+function textHttpRequest(file,callBack) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
