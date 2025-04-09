af68b7d95209858a869b64e33db2809a4188d4d9
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-09 02:10:50 +0200

    Update intro-page.js

--- IT_Skills/IntroPages/JS/intro-page.js
+++ IT_Skills/IntroPages/JS/intro-page.js
@@ -5,7 +5,7 @@ function introPageData(file,pageID) {
 function populateIntroPage(xml,arg1){
   var i;
   var xmlDoc = xml.responseXML;
-  var child=xmlDoc.getElementsByTagName("intro-page")["id"=arg1];
+  var child=xmlDoc.querySelector("intro-page["id"=arg1]");
   var imgSrc=child.getElementsByTagName("img")[0].childNodes[0].nodeValue; 
   document.querySelector("#intro-page-header img").setAttribute("src",imgSrc);
 }
