886e092de33ece8556a277f9b0e95193fbb9baf1
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-09 05:06:22 +0200

    Update intro-page.js

--- IT_Skills/IntroPages/JS/intro-page.js
+++ IT_Skills/IntroPages/JS/intro-page.js
@@ -22,7 +22,6 @@ function populateIntroPage(xml,arg1){
     for (i = 0; i < document.querySelectorAll("#toc-links a").length; i++) {
       document.querySelectorAll("#intro-page-toc a")[i].setAttribute("href",child.getElementsByTagName("a-href")[i].childNodes[0].nodeValue);
       document.querySelectorAll("#intro-page-toc a")[i].innerText=child.getElementsByTagName("a-text")[i].childNodes[0].nodeValue;      
-    } 
-    getXml("https://moulaytraining.github.io/MyStock/IT_Skills/IntroPages/CSS/intro-page.css",getIntroPageStyle,"");
+    }  
 }
 function getIntroPageStyle(xml,arg1){document.getElementById("intro-page-style").innerText = xml.responseText;}
