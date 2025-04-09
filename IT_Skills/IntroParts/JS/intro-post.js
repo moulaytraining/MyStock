113b3f69774c4dce4ef3135d67b60a3d9c9422f6
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-09 22:23:34 +0200

    Update intro-post.js

--- IT_Skills/IntroParts/JS/intro-post.js
+++ IT_Skills/IntroParts/JS/intro-post.js
@@ -6,10 +6,10 @@ function getIntroPostHtml(fileData,arg1){
     document.querySelector("#intro-post-header strong").innerText=child.getElementsByTagName("strong")[0].childNodes[0].nodeValue;
     document.querySelector("#intro-post-header img").setAttribute("src",child.getElementsByTagName("img")[0].childNodes[0].nodeValue);
     document.querySelector("#intro-post-introPara p").innerText=child.getElementsByTagName("p")[0].childNodes[0].nodeValue;
-    document.querySelector("#intro-post-toc h1").innerText=child.getElementsByTagName("h1")[0].childNodes[0].nodeValue; 
-    for (i = 0; i < document.querySelectorAll("#toc-links a").length; i++) {
-      document.querySelectorAll("#intro-post-toc a")[i].setAttribute("href",child.getElementsByTagName("a-href")[i].childNodes[0].nodeValue);
-      document.querySelectorAll("#intro-post-toc a")[i].innerText=child.getElementsByTagName("a-text")[i].childNodes[0].nodeValue;      
+    document.querySelector("#intro-post-lst01 h1").innerText=child.getElementsByTagName("h1")[0].childNodes[0].nodeValue; 
+    for (i = 0; i < document.querySelectorAll("#intro-post-lst01.lst-itms li").length; i++) {
+      document.querySelectorAll("intro-post-lst01.lst-itms li b")[i].innerText=child.getElementsByTagName("li-b")[i].childNodes[0].nodeValue; 
+      document.querySelectorAll("#intro-post-lst01.lst-itms li")[i].innerText=child.getElementsByTagName("li-text")[i].childNodes[0].nodeValue;      
     }  
 }
 function getIntroPostStyle(fileData,arg1){document.getElementById("intro-post-style").innerText = fileData.responseText;}
