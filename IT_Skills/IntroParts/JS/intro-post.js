b97ce40ac56cde9620d70ec1e277a11ecf20be7b
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-10 10:42:49 +0200

    Update intro-post.js

--- IT_Skills/IntroParts/JS/intro-post.js
+++ IT_Skills/IntroParts/JS/intro-post.js
@@ -24,7 +24,7 @@ function getIntroPostHtml(fileData,arg1){
       var normText=child.getElementsByTagName("li-text")[i].childNodes[0].nodeValue;   
       document.querySelectorAll("#intro-post-lst01>div>ol>li")[i].innerHTML="<b>"+bldText+"</b>"+normText;      
     }  
-    document.querySelector("#post-sec > div > p").innerText=child.getElementsByTagName("p")[1].childNodes[0].nodeValue;
+    document.querySelector("#intro-post > div.post-sec > div > p").innerText=child.getElementsByTagName("p")[1].childNodes[0].nodeValue;
     try{
         document.querySelector("#intro-post-lst01 > div > ol").removeChild(document.querySelector("#HTML202"));
         document.querySelector("article > div.separator").style.display="none";
