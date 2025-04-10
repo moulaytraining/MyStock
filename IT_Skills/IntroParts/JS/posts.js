270fab14421c4f39b6214489f8511faf95b59aab
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-11 01:41:44 +0200

    Update posts.js

--- IT_Skills/IntroParts/JS/posts.js
+++ IT_Skills/IntroParts/JS/posts.js
@@ -15,7 +15,7 @@ function getFileData(file,callBack,arg1) {
       for (i = 0; i < x.length; i++) {if(x[i].getAttribute("id") == arg1){var child=x[i]};}   
       document.querySelector("#post > header > strong").innerText=child.getElementsByTagName("strong")[0].childNodes[0].nodeValue;
       document.querySelector("#post > header > img").setAttribute("src",child.getElementsByTagName("img")[0].childNodes[0].nodeValue);
-      document.querySelector("#post-introPara p").innerText=child.getElementsByTagName("p")[0].childNodes[0].nodeValue;
+      document.querySelector(".post-introPara p").innerText=child.getElementsByTagName("p")[0].childNodes[0].nodeValue;
       document.querySelector(".post-o-lst h1").innerText=child.getElementsByTagName("h1")[0].childNodes[0].nodeValue;
       //console.log(document.querySelectorAll("#post-lst01 > div > ol > li").length);
       for (i = 0; i < document.querySelectorAll(".post-o-lst > div > ol > li").length; i++) {
