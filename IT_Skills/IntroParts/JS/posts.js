ff8cdfefdbd408e5105ee4006ba6dc2ea5f44d09
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-10 23:22:18 +0200

    Update posts.js

--- IT_Skills/IntroParts/JS/posts.js
+++ IT_Skills/IntroParts/JS/posts.js
@@ -13,8 +13,8 @@ function getFileData(file,callBack,arg1) {
       var xmlDoc = fileData.responseXML;  
       var x = xmlDoc.getElementsByTagName("post");
       for (i = 0; i < x.length; i++) {if(x[i].getAttribute("id") == arg1){var child=x[i]};}   
-      document.querySelector("#post-header strong").innerText=child.getElementsByTagName("strong")[0].childNodes[0].nodeValue;
-      document.querySelector("#post-header img").setAttribute("src",child.getElementsByTagName("img")[0].childNodes[0].nodeValue);
+      document.querySelector("#post > header > strong").innerText=child.getElementsByTagName("strong")[0].childNodes[0].nodeValue;
+      document.querySelector("#post > header > img").setAttribute("src",child.getElementsByTagName("img")[0].childNodes[0].nodeValue);
       document.querySelector("#post-introPara p").innerText=child.getElementsByTagName("p")[0].childNodes[0].nodeValue;
       document.querySelector(".post-lst h1").innerText=child.getElementsByTagName("h1")[0].childNodes[0].nodeValue;
       //console.log(document.querySelectorAll("#post-lst01 > div > ol > li").length);
