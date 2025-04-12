1324eefbc3e0d2b7a72c0c037f4e09823bc407fc
moulaytraining <133729317+moulaytraining@users.noreply.github.com>
2025-04-12 23:49:39 +0200

    Update posts.js

--- IT_Skills/IntroParts/JS/posts.js
+++ IT_Skills/IntroParts/JS/posts.js
@@ -38,7 +38,8 @@
           if(document.querySelectorAll(rptdImgs[i][0]).length!=0){
     //console.log(rptdImgs[i][0]);
         	for (j = 0; j < document.querySelectorAll(rptdImgs[i][0]).length; j++) {
-           		document.querySelectorAll(rptdImgs[i][0])[j].setAttribute("src",child.getElementsByTagName(rptdImgs[i][1])[j].childNodes[0].nodeValue);   
+           		document.querySelectorAll(rptdImgs[i][0])[j].setAttribute("src",child.getElementsByTagName(rptdImgs[i][1])[j].childNodes[0].nodeValue);  
+           		document.querySelectorAll(rptdImgs[i][0].Replace("img","a"))[j].setAttribute("src",child.getElementsByTagName(rptdImgs[i][1])[j].childNodes[0].nodeValue);    
            }
           }
         }
